e6f5a68db68a27ebe0ab31e2bd40b86e5440b553
JLamzon <jlamzon@yahoo.com>
2022-10-27 14:48:36 -0700

    added notes

--- Controllers/RestaurantPickerController.cs
+++ Controllers/RestaurantPickerController.cs
@@ -2,7 +2,7 @@
 //10-26-22
 //Restaurant Picker End Point
 //https://localhost:7087/restaurantpicker/foodpicker/*enter type of food*
-//Peer review:
+//Peer review by Carlos Felipe : This code is very organized. Looks polished. The inputs vary a lot and I liked the choice to be able to pick a random thing. Great work.
 
 
 using Microsoft.AspNetCore.Mvc;
